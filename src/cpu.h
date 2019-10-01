9d0fa7d8c952dd7932514bb37e5a0fe8a89777a2
Marshall Clyburn <mardev2013@gmail.com>
2019-09-30 21:43:58 -0400

    Add low-power modes

--- src/cpu.h
+++ src/cpu.h
@@ -8,6 +8,30 @@
 
 namespace mardev::msp430::cpu
 {
+    inline void enter_lpm0()
+    {
+        __asm__ __volatile__ ("bis #24, SR");
+    }
+
+    inline void enter_lpm1()
+    {
+        __asm__ __volatile__ ("bis #88, SR");
+    }
+
+    inline void enter_lpm2()
+    {
+        __asm__ __volatile__ ("bis #152, SR");
+    }
+
+    inline void enter_lpm3()
+    {
+        __asm__ __volatile__ ("bis #216, SR");
+    }
+
+    inline void enter_lpm4()
+    {
+        __asm__ __volatile__ ("bis #248, SR");
+    }
 }
 
 #endif
